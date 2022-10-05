d4b8b094899c7d5f16ae2450c403c822e4caab6f
Anderson Junior Rodrigues <andersonkz@ymail.com>
2022-10-04 21:29:42 -0300

    Testes unitários

---
+++ src/test/kotlin/com/example/ports/repository/BookRepositoryTest.kt
@@ -0,0 +1,9 @@
+package com.example.ports.repository
+
+import io.kotest.core.spec.style.FunSpec
+import io.micronaut.test.extensions.kotest.annotation.MicronautTest
+
+@MicronautTest
+class BookRepositoryTest(): FunSpec({
+
+})
